f55edd8e5aad8f75757af68941c09bf21ff74d41
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-14 10:22:19 +0200

    Add main.adb for Chandra-Toueg algorithm module

---
+++ main.adb
@@ -0,0 +1,9 @@
+-- main.adb
+with Ada.Text_IO; use Ada.Text_IO;
+with Chandra_Toueg;
+
+procedure Main is
+begin
+   Put_Line ("Chandra-Toueg Algorithm Module.");
+   Put_Line ("Please run the test suite via 'make test' to verify algorithm behavior.");
+end Main;
